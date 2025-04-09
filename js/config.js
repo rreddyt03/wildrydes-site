45a7b1392fba30d1d49f080865f9564737e805b6
Rishi Reddy Thokala <rreddyt03@gmail.com>
2025-04-09 12:54:12 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
   cognito: {
-    userPoolId: 'us-east-1_vhxfr', // replace with your actual User Pool ID
-    userPoolClientId: '222m0oreau8pkg5sfk1mlkpp6g',
+    userPoolId: 'us-east-1_Ru4QzquYK',
+    userPoolClientId: '1qfip991fgmtamptuar6r250ts',
     region: 'us-east-1'
   },
   api: {
-    invokeUrl: '' // Leave blank for now — you'll fill this in Step 12
+    invokeUrl: ''
   }
 };
